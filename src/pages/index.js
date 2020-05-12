a216fec9d1c92c826e155868b3332f2ecac5cd97
zottwickel <scott.g.mckell@gmail.com>
2020-05-12 16:47:06 -0600

    add gallery

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import SEO from "../components/seo"
 import { HomeHeader, Banner, BannerButton } from "../utils"
 import img from "../images/bcg/homeBcg.jpeg"
 import QuickInfo from "../components/HomePageComponents/QuickInfo"
+import Gallery from "../components/HomePageComponents/Gallery"
 
 const IndexPage = () => (
   <Layout>
@@ -14,6 +15,7 @@ const IndexPage = () => (
       </Banner>
     </HomeHeader>
     <QuickInfo />
+    <Gallery />
   </Layout>
 )
 export default IndexPage
